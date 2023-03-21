03a8e6dd7fc964011872ff3fd759c615d3804758
xkollar2 <62118653+xkollar2@users.noreply.github.com>
2023-03-21 12:59:20 +0100

    WIP

--- csharp/TheatricalPlayersRefactoringKata/Performance.cs
+++ csharp/TheatricalPlayersRefactoringKata/Performance.cs
@@ -4,15 +4,30 @@ namespace TheatricalPlayersRefactoringKata
     {
         private string _playID;
         private int _audience;
+        private int _amount;
 
-        public string PlayID { get => _playID; set => _playID = value; }
-        public int Audience { get => _audience; set => _audience = value; }
+        public int Amount
+        {
+            get => _amount;
+            set => _amount = value;
+        }
+
+        public string PlayID
+        {
+            get => _playID;
+            set => _playID = value;
+        }
+
+        public int Audience
+        {
+            get => _audience;
+            set => _audience = value;
+        }
 
         public Performance(string playID, int audience)
         {
             this._playID = playID;
             this._audience = audience;
         }
-
     }
 }
--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,23 +6,27 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
-
-
         public string PrintAsHtml(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
             var volumeCredits = 0;
             var result = "<html>\n";
-            result += string.Format("<h1>Statement for {0}</h1>\n", invoice.Customer);
+            result += string.Format("  <h1>Statement for {0}</h1>\n", invoice.Customer);
+            result += "  <table>";
+            result += "    <tr><th>play</th><th>seats</th><th>cost</th></tr>";
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
             CalculatePerformanceCost(invoice, plays, ref totalAmount, ref volumeCredits, ref result, cultureInfo);
             foreach (var perf in invoice.Performances)
             {
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", plays[perf.PlayID].Name, Convert.ToDecimal(perf.Amount / 100), perf.Audience);
+                result += string.Format(cultureInfo, "<tr><td>{0}: {1:C} ({2} seats)\n", plays[perf.PlayID].Name,
+                    Convert.ToDecimal(perf.Amount / 100), perf.Audience);
             }
-            result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += string.Format("You earned {0} credits\n", volumeCredits);
+
+            result += string.Format(cultureInfo, "<p>Amount owed is {0:C}</p>\n", Convert.ToDecimal(totalAmount / 100));
+            result += string.Format("<p>You earned {0} credits\n</p>", volumeCredits);
+            result += "</html>";
+            
             return result;
         }
 
@@ -37,14 +41,17 @@ namespace TheatricalPlayersRefactoringKata
             CalculatePerformanceCost(invoice, plays, ref totalAmount, ref volumeCredits, ref result, cultureInfo);
             foreach (var perf in invoice.Performances)
             {
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", plays[perf.PlayID].Name, Convert.ToDecimal(perf.Amount / 100), perf.Audience);
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", plays[perf.PlayID].Name,
+                    Convert.ToDecimal(perf.Amount / 100), perf.Audience);
             }
+
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
             return result;
         }
 
-        private static void CalculatePerformanceCost(Invoice invoice, Dictionary<string, Play> plays, ref int totalAmount, ref int volumeCredits, ref string result, CultureInfo cultureInfo)
+        private static void CalculatePerformanceCost(Invoice invoice, Dictionary<string, Play> plays,
+            ref int totalAmount, ref int volumeCredits, ref string result, CultureInfo cultureInfo)
         {
             foreach (var perf in invoice.Performances)
             {
@@ -57,6 +64,7 @@ namespace TheatricalPlayersRefactoringKata
                         {
                             perf.Amount += 1000 * (perf.Audience - 30);
                         }
+
                         break;
                     case "comedy":
                         perf.Amount = 30000;
@@ -64,11 +72,13 @@ namespace TheatricalPlayersRefactoringKata
                         {
                             perf.Amount += 10000 + 500 * (perf.Audience - 20);
                         }
+
                         perf.Amount += 300 * perf.Audience;
                         break;
                     default:
                         throw new Exception("unknown type: " + play.Type);
                 }
+
                 // add volume credits
                 volumeCredits += Math.Max(perf.Audience - 30, 0);
                 // add extra credit for every ten comedy attendees
@@ -78,6 +88,5 @@ namespace TheatricalPlayersRefactoringKata
                 totalAmount += perf.Amount;
             }
         }
-
     }
 }
